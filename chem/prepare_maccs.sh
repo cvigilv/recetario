7defa8c785ee66b70602f394087add2e5ad18ca5
Carlos Vigil Vásquez <cvigil2@uc.cl>
2022-08-24 00:46:59 -0400

    feat(chem): added named indices for MACCS fingerprints

--- chem/prepare_maccs.sh
+++ chem/prepare_maccs.sh
@@ -2,23 +2,28 @@
 #title           :prepare_maccs.sh
 #description     :Calculate MACCS molecular descriptor
 #author          :Carlos Vigil Vásquez
-#date            :20220511
-#version         :20220511a
+#date            :20220829
+#version         :20220829a
 #notes           :Requires obabel
 #copyright       :Copyright (C) 2022 Carlos Vigil Vásquez (cvigil2@uc.cl).
 #license         :Permission to copy and modify is granted under the MIT license
 
 SMILES=$1
-MACCS="$(echo $SMILES | sed s/SMILES/DS/ | sed s/smi/maccs.txt/)"
+MACCS="$(echo "$SMILES" | sed s/SMILES/DS/ | sed s/smi/maccs.txt/)"
 
 echo "Calculating OpenBabel's MACCS fingerprint for $1"
 
 # Calculate and clean-up descriptor
-obabel -ismi "$SMILES" -ofpt -O "/tmp/$(basename $MACCS)" -xh -xfMACCS
-cat "/tmp/$(basename $MACCS)" |\
-	grep -v 'Possible superstructure of' |\
-	grep -v '>' | paste -d '\0' - - | sed s/' '//g | \
-	tr "[a-z]" "[A-Z]" | \
+obabel -ismi "$SMILES" -ofpt -O "/tmp/$(basename "$MACCS")" -xh -xfMACCS
+
+# Retrieve compound names
+grep '^>' "/tmp/$(basename "$MACCS")" |\
+	sed "s/^>\(\w\+\).*/\"\1\"/g" > "/tmp/$(basename "$MACCS")_names"
+
+# Convert hex to binary
+grep -v 'Possible superstructure of' "/tmp/$(basename "$MACCS")" |\
+	grep -v '>' | paste -d '\0'  - - |\
+	sed s/' '//g | tr "[a-z]" "[A-Z]" | \
 	sed s/0/0000/g | \
 	sed s/1/0001/g | \
 	sed s/2/0010/g | \
@@ -36,4 +41,5 @@ cat "/tmp/$(basename $MACCS)" |\
 	sed s/E/1110/g | \
 	sed s/F/1111/g | \
 	sed 's/./& /g' | \
-	sed 's/[[:space:]]*$//' > "$MACCS"
+	sed 's/[[:space:]]*$//' > "/tmp/$(basename "$MACCS")_binary"
+paste -d" " "/tmp/$(basename "$MACCS")_names" "/tmp/$(basename "$MACCS")_binary" > "$MACCS"
