64f5254270a356249c5bbe67b7537c969137d7a2
Carlos Vigil Vásquez <cvigil2@uc.cl>
2022-09-02 11:40:42 -0400

    fix(chem): minor bug bixes

--- chem/calculate_shape.py
+++ chem/calculate_shape.py
@@ -12,6 +12,7 @@ import argparse
 import itertools
 import multiprocessing
 import numpy as np
+import pandas as pd
 from tqdm import tqdm
 from rdkit import Chem
 from rdkit.Chem import rdMolAlign, rdShapeHelpers, rdMolDescriptors, AllChem
@@ -67,13 +68,16 @@ def Crippen_ShapeTversky(ref, query, ref_params, query_params, conf_ids, alpha, 
 
     return similarity
 
+
 def main():
     print(f"{__title__} v{__version__} - {__desc__}\n")
     # Argument parser {{{
     parser = argparse.ArgumentParser(description="Calculate shape similarity matrix")
     parser.add_argument("-i", "--input", type=str, nargs="+", help="SMILES files")
-    parser.add_argument("-o", "--output", type=str, default='shapesimmat.txt', help="Similarity matrix")
-    parser.add_argument("--delim", type=str, default=' ', help="SMILES files delimiter")
+    parser.add_argument(
+        "-o", "--output", type=str, default="shapesimmat.txt", help="Similarity matrix"
+    )
+    parser.add_argument("--delim", type=str, default=" ", help="SMILES files delimiter")
     parser.add_argument("--alpha", type=float, default=1.0, help="Tversky α parameter")
     parser.add_argument("--beta", type=float, default=1.0, help="Tversky β parameter")
     parser.add_argument(
@@ -82,9 +86,18 @@ def main():
         default="crippen",
         help="Alignment method (options: ['crippen', 'mmfp'])",
     )
-    parser.add_argument("--nconfs", type=int, default=25, help="Number of conformers per compounds")
-    parser.add_argument("--prunerms", type=float, default=0.5, help="Conformer prune RMS")
-    parser.add_argument("--nthreads", type=int, default=1, help="Number of threads to use ('-1' for all threads)")
+    parser.add_argument(
+        "--nconfs", type=int, default=25, help="Number of conformers per compounds"
+    )
+    parser.add_argument(
+        "--prunerms", type=float, default=0.5, help="Conformer prune RMS"
+    )
+    parser.add_argument(
+        "--nthreads",
+        type=int,
+        default=1,
+        help="Number of threads to use ('-1' for all threads)",
+    )
 
     args = parser.parse_args()
 
@@ -95,37 +108,42 @@ def main():
         exit(1)
 
     if args.method not in ["crippen", "mmfp"]:
-        print(f"[FATAL] Method {args.method} not available! Try with either 'crippen' or 'mmfp'")
+        print(
+            f"[FATAL] Method {args.method} not available! Try with either 'crippen' or 'mmfp'"
+        )
         exit(1)
 
     if args.nthreads == -1:
         args.nthreads = multiprocessing.cpu_count()
 
-
     print(":: Configuration ::")
     print("===================")
-    for item,value in vars(args).items():
+    for item, value in vars(args).items():
         if type(value) is list:
-            value = '; '.join(value)
+            value = "; ".join(value)
         print(f"{item.ljust(10)} = {value}")
     print()
-#}}}
+    # }}}
 
-# Conformer generation
+    # Conformer generation
     print(":: Conformer generation ::")
     print("==========================")
     ref_mols = [
         Chem.AddHs(m)
-        for m in Chem.SmilesMolSupplier(args.input[0], delimiter=args.delim)
+        for m in Chem.SmilesMolSupplier(args.input[0], delimiter=args.delim, titleLine=False)
         if m != None
     ]
-    for mol in tqdm(ref_mols, desc=f"Calculating {args.nconfs} conformers for reference molecules"):
+    for mol in tqdm(
+        ref_mols,
+        desc=f"Calculating {args.nconfs} conformers for reference molecules",
+        smoothing=0.0,
+    ):
         AllChem.EmbedMultipleConfs(
             mol,
-            clearConfs = True,
-            numConfs = args.nconfs,
-            numThreads = args.nthreads,
-            pruneRmsThresh=args.prunerms
+            clearConfs=True,
+            numConfs=args.nconfs,
+            numThreads=args.nthreads,
+            pruneRmsThresh=args.prunerms,
         )
 
     if len(args.input) == 1:
@@ -133,42 +151,70 @@ def main():
     else:
         query_mols = [
             Chem.AddHs(m)
-            for m in Chem.SmilesMolSupplier(args.input[1], delimiter=args.delim)
+            for m in Chem.SmilesMolSupplier(args.input[1], delimiter=args.delim, titleLine=False)
             if m != None
         ]
-        for mol in tqdm(query_mols, desc=f"Calculating {args.nconfs} conformers for query molecules    "):
+        for mol in tqdm(
+            query_mols,
+            desc=f"Calculating {args.nconfs} conformers for query molecules    ",
+            smoothing=0.0,
+        ):
             AllChem.EmbedMultipleConfs(
                 mol,
-                clearConfs = True,
-                numConfs = args.nconfs,
-                numThreads = args.nthreads,
-                pruneRmsThresh=args.prunerms
+                clearConfs=True,
+                numConfs=args.nconfs,
+                numThreads=args.nthreads,
+                pruneRmsThresh=args.prunerms,
             )
 
-# Alignment and similarity calculation
+    conformer_distribution = [mol.GetNumConformers() for mol in ref_mols] + [
+        mol.GetNumConformers() for mol in query_mols
+    ]
+    print("Descriptive statistics:")
+    print(f"{' - Number of reference molecules'.ljust(40)} = {len(ref_mols)}")
+    if len(args.input) == 2:
+        print(f"{' - Number of query molecules'.ljust(40)} = {len(query_mols)}")
+    print("Number of conformers per molecule:")
+    print(
+        f"{' - Mean ± SD'.ljust(40)} = {np.nanmean(conformer_distribution):.2f} ± {np.nanstd(conformer_distribution):.2f}"
+    )
+    print(
+        f"{' - Median (IQR)'.ljust(40)} = {np.nanmedian(conformer_distribution):.0f} ({np.nanpercentile(conformer_distribution, 25):.0f} - {np.nanpercentile(conformer_distribution, 75):.0f})"
+    )
+
+    # Alignment and similarity calculation
     print()
     print(":: Shape similarity calculation ::")
     print("==================================")
 
+    # Initialization
     shape_func = None
     ref_params = []
     query_params = []
 
-    match args.method:
-        case "mmfp":
-            shape_func = MMFP_ShapeTversky
-            ref_params = [AllChem.MMFFGetMoleculeProperties(mol) for mol in ref_mols]
-            query_params = [AllChem.MMFFGetMoleculeProperties(mol) for mol in query_mols]
-        case "crippen":
-            shape_func = Crippen_ShapeTversky
-            ref_params = [rdMolDescriptors._CalcCrippenContribs(mol) for mol in ref_mols]
-            query_params = [rdMolDescriptors._CalcCrippenContribs(mol) for mol in query_mols]
+    # Parameter calculation
+    if args.method == "mmfp":
+        shape_func = MMFP_ShapeTversky
+        ref_params = [AllChem.MMFFGetMoleculeProperties(mol) for mol in ref_mols]
+        query_params = [AllChem.MMFFGetMoleculeProperties(mol) for mol in query_mols]
+    elif args.method == "crippen":
+        shape_func = Crippen_ShapeTversky
+        ref_params = [rdMolDescriptors._CalcCrippenContribs(mol) for mol in ref_mols]
+        query_params = [
+            rdMolDescriptors._CalcCrippenContribs(mol) for mol in query_mols
+        ]
 
+    # Similarity matrix calculation
     simmat = np.zeros((len(ref_mols), len(query_mols)))
-
     pool = multiprocessing.Pool(args.nthreads)
+
     if len(args.input) == 1 and (args.alpha == args.beta):
-        for i, ref in tqdm(list(enumerate(ref_mols)), desc="Calculating Shape similarity"):
+        pbar = tqdm(
+            total=int(len(ref_mols) * (len(ref_mols) - 1) / 2),
+            desc="Calculating shape similarity",
+            smoothing=0.0,
+        )
+        for i, ref in enumerate(ref_mols):
             for j, query in enumerate(ref_mols[0:i]):
                 if ref == query:
                     all_tanimotos = [1]
@@ -178,26 +224,36 @@ def main():
 
                     # Parallelized Shape Tanimoto for every possible pair of conformers
                     # between reference and query molecule
-                    all_tanimotos = pool.starmap(
+                    for similarity in pool.starmap(
                         shape_func,
                         zip(
                             itertools.repeat(ref),
                             itertools.repeat(query),
                             itertools.repeat(ref_params[i]),
                             itertools.repeat(query_params[j]),
-                            itertools.product(range(ref.GetNumConformers()), range(query.GetNumConformers())),
+                            itertools.product(
+                                range(ref.GetNumConformers()),
+                                range(query.GetNumConformers()),
+                            ),
                             itertools.repeat(args.alpha),
                             itertools.repeat(args.beta),
-                           )
-                    )
+                        ),
+                    ):
+                        all_tanimotos.append(similarity)
 
                 # Assign max(ShapeTanimoto) to molecule pair
-                simmat[i,j] = np.max(all_tanimotos)
+                simmat[i, j] = np.max(all_tanimotos)
+                pbar.update(1)
 
         simmat = np.maximum(simmat, simmat.transpose())
 
     else:
-        for i, ref in tqdm(list(enumerate(ref_mols)), desc="Calculating Shape similarity"):
+        pbar = tqdm(
+            total=len(ref_mols) * len(query_mols),
+            desc="Calculating shape similarity",
+            smoothing=0.0,
+        )
+        for i, ref in enumerate(ref_mols):
             for j, query in enumerate(query_mols):
                 if ref == query:
                     all_tanimotos = [1]
@@ -207,29 +263,42 @@ def main():
 
                     # Parallelized Shape Tanimoto for every possible pair of conformers
                     # between reference and query molecule
-                    all_tanimotos = pool.starmap(
+                    for similarity in pool.starmap(
                         shape_func,
                         zip(
                             itertools.repeat(ref),
                             itertools.repeat(query),
                             itertools.repeat(ref_params[i]),
                             itertools.repeat(query_params[j]),
-                            itertools.product(range(ref.GetNumConformers()), range(query.GetNumConformers())),
+                            itertools.product(
+                                range(ref.GetNumConformers()),
+                                range(query.GetNumConformers()),
+                            ),
                             itertools.repeat(args.alpha),
                             itertools.repeat(args.beta),
-                           )
-                    )
+                        ),
+                    ):
+                        all_tanimotos.append(similarity)
 
                 # Assign max(ShapeTanimoto) to molecule pair
-                simmat[i,j] = np.max(all_tanimotos)
+                simmat[i, j] = np.max(all_tanimotos)
+                pbar.update(1)
+    pbar.close()
 
+    print(
+        f"{'Mean ± SD'.ljust(15)} = {np.nanmean(simmat.flatten()):.3f} ± {np.nanstd(simmat.flatten()):.3f}"
+    )
+    print(
+        f"{'Median (IQR)'.ljust(15)} = {np.nanmedian(simmat.flatten()):.3f} ({np.nanpercentile(simmat.flatten(), 25):.3f} - {np.nanpercentile(simmat.flatten(), 75):.3f})"
+    )
 
-    print()
-    print(":: Shape similarity description ::")
-    print("==================================")
+    simmat_df = pd.DataFrame(simmat)
+    simmat_df.index = [mol.GetProp("_Name") for mol in ref_mols]
+    simmat_df.columns = [mol.GetProp("_Name") for mol in query_mols]
+    print(simmat_df)
 
-    print(f"{'Mean ± SD'.ljust(15)} = {np.nanmean(simmat.flatten()):.3f} ± {np.nanstd(simmat.flatten()):.3f}")
-    print(f"{'Median (IQR)'.ljust(15)} = {np.nanmedian(simmat.flatten()):.3f} ({np.nanpercentile(simmat.flatten(), 25):.3f} - {np.nanpercentile(simmat.flatten(), 75):.3f})")
+    simmat_df.to_csv(args.output, header=True, index=True, sep=args.delim)
+
 
 
 if __name__ == "__main__":
