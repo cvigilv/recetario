e1d38238dee01993c7bd0b8c69c8e1df46232153
Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>
2025-03-06 17:20:34 +0100

    refactor(chem): make use of `uv tool`
    - Replaced outdated script header with PEP 621 compliant metadata
    - Updated dependencies specification
    - Modified command-line argument handling for output file
    - Removed unnecessary comments and whitespace

--- chem/prepare_fcfp4.py
+++ chem/prepare_fcfp4.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-#title           :prepare_fcfp4.py
-#description     :Calculate FCFP4 molecular descriptor
-#author          :Carlos Vigil Vásquez
-#date            :20220511
-#version         :20220511a
-#notes           :Requires rdkit, numpy & pandas
-#copyright       :Copyright (C) 2022 Carlos Vigil Vásquez (cvigil2@uc.cl).
-#license         :Permission to copy and modify is granted under the MIT license
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "numpy",
+#     "pandas",
+#     "rdkit",
+# ]
+# ///
 
 import sys
 
@@ -16,7 +16,7 @@ from rdkit.Chem import SmilesMolSupplier
 from rdkit.Chem.AllChem import GetMorganFingerprintAsBitVect
 
 SMILES = sys.argv[1]
-FCFP4 = SMILES.replace("SMILES", "DS").replace("smi", "fcfp4.txt")
+FCFP4 = sys.argv[2]
 
 print(f"Calculating RDKit's FCFP4 fingerprint for {SMILES}")
 
@@ -27,6 +27,7 @@ all_smiles = SmilesMolSupplier(SMILES, delimiter="\t\t", titleLine=False)
 args = dict(useChirality=True, radius=2, nBits=2048, useFeatures=True)
 bits = [GetMorganFingerprintAsBitVect(molecule, **args) for molecule in all_smiles]
 bits = [array(fingerprint) for fingerprint in bits]
+
 
 # Save fingerprint
 DataFrame(bits).to_csv(FCFP4, sep=" ", header=False, index=False)
